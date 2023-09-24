b5d318407cda5cee9f714b59ec4e5f9c26f41119
Daniel Wilson <daniel1.wilson@intel.com>
2023-09-24 06:17:13 -0700

    Reduce test log noise in MSRIOGroupTest
    - Suppress warnings about unexpected mock calls to add_read and
      add_write in MSRIOGroupTest.
    - Removes about 57 MB of noise from test logs

--- libgeopmd/test/MSRIOGroupTest.cpp
+++ libgeopmd/test/MSRIOGroupTest.cpp
@@ -44,6 +44,7 @@ using geopm::MSRIOGroup;
 using geopm::PlatformTopo;
 using geopm::Exception;
 using geopm::MSR;
+using testing::AnyNumber;
 using testing::Return;
 using testing::SetArgReferee;
 using testing::_;
@@ -129,6 +130,9 @@ void MSRIOGroupTest::SetUp()
     // suppress mock calls from initializing rdt signals
     EXPECT_CALL(*m_msrio, read_msr(_, _)).Times(AtLeast(0));
     EXPECT_CALL(*m_mock_cpuid, cpuid()).WillRepeatedly(Return(MSRIOGroup::M_CPUID_SKX));
+    // suppress mock calls from initializing MSR fields from JSON config
+    EXPECT_CALL(*m_msrio, add_read(_, _, _)).Times(AnyNumber());
+    EXPECT_CALL(*m_msrio, add_write(_, _, _)).Times(AnyNumber());
     m_msrio_group = geopm::make_unique<MSRIOGroup>(*m_topo, m_msrio,
                                                    m_mock_cpuid,
                                                    m_num_cpu,
