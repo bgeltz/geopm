e7cc959926bbb7f83084d7eaca77a913a3505031
Brad Geltz <brad.geltz@intel.com>
2024-04-03 14:31:03 -0700

    Move version and archive settings to helper script
    - Required for OBS.
    
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- geopmdpy/make_rpm.sh
+++ geopmdpy/make_rpm.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-set -x -e
+set -xe
 
 # Build source distribution
 python3 -m build --sdist | tee make-sdist.log
 ARCHIVE=$(cat make-sdist.log | tail -n 1 | sed 's|^Successfully built ||')
+VERSION=$(python3 -c "from setuptools_scm import get_version; print(get_version('..'))")
 
-RPM_TOPDIR=${RPM_TOPDIR:-${HOME}/rpmbuild}
-mkdir -p ${RPM_TOPDIR}/SOURCES
-mkdir -p ${RPM_TOPDIR}/SPECS
-cp dist/${ARCHIVE} ${RPM_TOPDIR}/SOURCES
-cp geopmdpy.spec ${RPM_TOPDIR}/SPECS
-rpmbuild --define "archive ${ARCHIVE}" -ba ${RPM_TOPDIR}/SPECS/geopmdpy.spec
+sed -e "s/@ARCHIVE@/${ARCHIVE}/" -e "s/@VERSION@/${VERSION}/" geopmdpy.spec.in > geopmdpy.spec
+
+# On systems where rpmbuild is not available (i.e. GitHub CI builder used to create
+# the distribution tarball that is uploaded to OBS) the following will be skipped.
+if [ -x "$(command -v rpmbuild)" ]; then
+    RPM_TOPDIR=${RPM_TOPDIR:-${HOME}/rpmbuild}
+    mkdir -p ${RPM_TOPDIR}/SOURCES
+    mkdir -p ${RPM_TOPDIR}/SPECS
+    cp dist/${ARCHIVE} ${RPM_TOPDIR}/SOURCES
+    cp geopmdpy.spec ${RPM_TOPDIR}/SPECS
+    rpmbuild --define "archive ${ARCHIVE}" -ba ${RPM_TOPDIR}/SPECS/geopmdpy.spec
+fi
