9f9bd91f0013c2377166a08c5b441c312e43977e
Brad Geltz <brad.geltz@intel.com>
2024-06-13 14:03:43 -0700

    Setup test_multi_app.sh for a large scale run
    - Traces are disabled to avoid overwhelming the
      filesystem.
    - geopmctl utilizes MPI to collect remote reports.
    - geopmbench has MPI disabled.
    - Extend py test timeout.
    - Increase app runtime via loop-count and complexity.
    - Adjust test expectations to account for increased app loop-count.
    - Extend DBus timeout.
    
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- integration/test/test_multi_app.py
+++ integration/test/test_multi_app.py
@@ -22,10 +22,9 @@ from integration.test import util
 
 class TestIntegration_multi_app(unittest.TestCase):
     TEST_NAME = 'test_multi_app'
-    TIME_LIMIT = 60
-    NUM_NODE = 1
-    EXPECTED_REGIONS = {'model-init', 'stream', 'dgemm',
-                        'MPI_Init_thread'}
+    TIME_LIMIT = 120
+    NUM_NODE = util.get_num_node()
+    EXPECTED_REGIONS = {'model-init', 'stream', 'dgemm'}
 
     @classmethod
     def setUpClass(cls):
@@ -33,10 +32,11 @@ class TestIntegration_multi_app(unittest.TestCase):
                          '.' + cls.__name__ + ') ...')
         script_dir = os.path.dirname(os.path.realpath(__file__))
         script_path = os.path.join(script_dir,'test_multi_app.sh')
-        subprocess.run(['/bin/bash', script_path],
-                       timeout=cls.TIME_LIMIT, check=True)
+        if util.do_launch():
+            subprocess.run(['/bin/bash', script_path],
+                           timeout=cls.TIME_LIMIT, check=True)
 
-        cls._report_path = f'{cls.TEST_NAME}_report.yaml-{gethostname()}'
+        cls._report_path = f'{cls.TEST_NAME}_report.yaml'
         cls._report = geopmpy.io.RawReport(cls._report_path)
         cls._node_names = cls._report.host_names()
 
@@ -46,7 +46,8 @@ class TestIntegration_multi_app(unittest.TestCase):
     def test_expected_regions_exist(self):
         for node in self._node_names:
             regions = set(self._report.region_names(node))
-            self.assertEqual(regions, self.EXPECTED_REGIONS)
+            for rr in self.EXPECTED_REGIONS:
+                self.assertIn(rr, regions, msg=node)
 
     def test_regions_valid(self):
         for node in self._node_names:
@@ -55,7 +56,7 @@ class TestIntegration_multi_app(unittest.TestCase):
                 if region in ('model-init', 'MPI_Init_thread'):
                     self.assertEqual(region_data['count'], 0.5)
                 else:
-                    self.assertEqual(region_data['count'], 1)
+                    self.assertEqual(region_data['count'], 25)
                 if region != 'MPI_Init_thread':
                     # We do not sample during PMPI_Init_thread call
                     # but other regions should have non-zero sample time
@@ -65,7 +66,7 @@ class TestIntegration_multi_app(unittest.TestCase):
     def test_non_mpi_app_tracked(self):
         for node in self._node_names:
             unmarked_data = self._report.raw_unmarked(node)
-            self.assertGreater(unmarked_data['TIME@package-1'], 0)
+            self.assertGreater(unmarked_data['TIME@package-1'], 0, msg=node)
 
     def test_runtime(self):
         for node in self._node_names:
--- integration/test/test_multi_app.sh
+++ integration/test/test_multi_app.sh
@@ -3,33 +3,43 @@
 #  SPDX-License-Identifier: BSD-3-Clause
 #
 
-cat > temp_config.json << "EOF"
+mkdir -p ${TMPDIR}
+
+INPUT_FILE=$(mktemp)
+cat > ${INPUT_FILE} << "EOF"
 {
-    "loop-count": 2,
+    "loop-count": 50,
     "region": ["stream", "dgemm"],
-    "big-o": [1.0, 10.0]
+    "big-o": [3.0, 30.0]
 }
 EOF
-
-source ${GEOPM_SOURCE}/integration/config/run_env.sh
 
 TEST_NAME=test_multi_app
 export GEOPM_PROFILE=${TEST_NAME}
 export GEOPM_PROGRAM_FILTER=geopmbench,stress-ng
 export LD_PRELOAD=libgeopm.so.2.1.0
 
+export SYSTEMD_BUS_TIMEOUT=600
+
 GEOPM_REPORT=${TEST_NAME}_report.yaml \
-GEOPM_TRACE=${TEST_NAME}_trace.csv \
-GEOPM_TRACE_PROFILE=${TEST_NAME}_trace_profile.csv \
 GEOPM_REPORT_SIGNALS=TIME@package \
 GEOPM_NUM_PROC=2 \
-GEOPM_CTL_LOCAL=true \
 setsid geopmctl &
 
+# Some of the initialization regions of geopmbench finish quickly. The
+# test_multi_app.py test checks the execution time of all regions. This sleep
+# ensures that geopmctl is started before starting geopmbench.
+sleep 2
+
+echo "$(hostname) starting..."
 # geopmbench
-numactl --cpunodebind=0 -- geopmbench temp_config.json &
+export GEOPMBENCH_NO_MPI=1
+/usr/bin/timeout -v -k 30s 18m numactl --cpunodebind=0 -- /usr/bin/timeout -v -k 30s 15m geopmbench ${INPUT_FILE} &
 
 # stress-ng
-numactl --cpunodebind=1 -- stress-ng --cpu 1 --timeout 10 &
+export LD_PRELOAD=libgeopm.so.2.1.0
+numactl --cpunodebind=1 -- stress-ng --cpu 1 --timeout 120 &
 
-wait $(jobs -p)
+wait
+rm ${INPUT_FILE}
+echo "$(hostname) complete"
